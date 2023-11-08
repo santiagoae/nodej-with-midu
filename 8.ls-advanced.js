3466e1514161ffde924b70915a2eb6f2002ce016
gcsantiagoae <savendanoe@garantiascomunitarias.com>
2023-11-08 17:35:40 -0500

    video 1 1:55

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
+const pc = require('picocolors');
 
 const folder = process.argv[2] ?? '.';
 console.log(folder);
@@ -8,7 +9,7 @@ async function ls(folder){
     try {
         files = await fs.readdir(folder)
     } catch {
-        console.error(`no se ha podido leer el directorio ${folder}`)
+        console.error(pc.red(`⛔ no se ha podido leer el directorio ${folder}`))
         process.exit(1);
     }
 
@@ -27,7 +28,7 @@ async function ls(folder){
         const fileSize = stats.size
         const fileModified = stats.mtime.toLocaleDateString()
 
-        return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
+        return `${fileType} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.toString().padStart(10))} ${pc.yellow(fileModified)}`
     })
 
     const filesInfo = await Promise.all(filesPromises);
